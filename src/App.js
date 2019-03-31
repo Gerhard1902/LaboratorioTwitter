2c546c442ed1783cd0f49b66e3e7839d3fd3b77d
Gerhard1902 <32085719+Gerhard1902@users.noreply.github.com>
2019-03-31 15:33:35 -0600

    Usando URL de Heroku

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ class Bar extends Component{ // function sintax
 }
 
 
+
+
+
 class InputTweet extends Component{ // function sintax
   constructor(props){
     super(props);
@@ -27,6 +30,8 @@ class InputTweet extends Component{ // function sintax
    handleChange =(e)=>{
    	this.setState({text: e.target.value}) 
 }
+
+
 handleClick=(e)=>{
 	
 	this.props.handleClick(e, this.myRef);
@@ -43,15 +48,20 @@ handleClick=(e)=>{
 }
 
 
+
+
+
+
 function Feed(props){
 	return(
 		props.tweets.map((step, index) => {
         return (
           <Tweets 
                 className="actualTweet"
-                name={step.name} 
-                date={step.date}
-                message={step.message} 
+                image={step.avatar}
+                name={step.user_name} 
+                date={step.created_at}
+                message={step.description} 
               />
         )
        })
@@ -60,15 +70,26 @@ function Feed(props){
 }
 
 
+
+
+
+
+
 function Tweets (props){ // function sintax
   return(
     <div className="tweet2">
-      <img className="llama" src={llama} />
-      <div className="column">
-        <div>{props.name}</div>
-        <div>{props.message}</div> 
+      
+      <div className="row">
+      	<img className="llama" src={props.image} />
+        <div>
+	        <div>{props.name}</div>
+	        
+	        <div>{props.message}</div> 
+        </div>
+
       </div>
-      <div className="date">{props.date}</div>
+       <div className="date">{props.date}</div>
+     
       
     </div>
   );
@@ -82,19 +103,63 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state={
-      tweets: []
+      tweets: [],
+      error:null,
+      isLoaded:false,
       };
       this.myRef= React.createRef();
   }
 
+  componentDidMount(){
+	fetch("https://still-garden-88285.herokuapp.com/draft_tweets")
+		.then(response=> response.json())
+		.then(
+			(result)=>{
+				console.log(result);
+				this.setState({
+					isLoaded:true,
+					tweets:result.draft_tweets
+				})
+			},
+			(error)=>{
+				this.setState({
+					isLoaded:true,
+					error:error
+				})
+			}
+		)
+}
+
+
+
 handleClick =(e, childRef)=>{
  	console.log(e,childRef);
-   const tweets = this.state.tweets.slice();
-   childRef.current.focus();
-    this.setState({
-      tweets: [...tweets, {name:"Gerardo", date:"11-03-2019", message:childRef.current.value} ],
-    } );
-    childRef.current.value="";
+ 	childRef.current.focus();
+ 	const post2={
+ 		
+ 	};
+  
+    fetch("https://still-garden-88285.herokuapp.com/draft_tweets",
+    	{method:"POST", 
+    	body: JSON.stringify({
+    		avatar:"http://notiultimas.com/digital/wp-content/uploads/2018/11/Llamas.jpg",
+ 			user_name: "anonymous user",
+ 			description: childRef.current.value
+ 		}), 
+ 		headers:{'Content-Type': 'application/json'}})
+    .then(res => res.json())
+	.then((response) => //{
+	console.log('Success:', JSON.stringify({response}))
+	/*
+	 this.setState({
+					isLoaded:true,
+					tweets:response.draft_tweets
+				})}*/
+
+	)
+.catch(error => console.error('Error:', error));
+childRef.current.value="";
+    
     
 }
 
@@ -105,29 +170,39 @@ handleClick =(e, childRef)=>{
 
 
     render(){
-      const array = this.state.tweets;
-     
+     const {error, isLoaded, tweets}=this.state;
+     let content;
+     if(error){
+      		content=<div>Error:{error.message}</div>;
+      	} else {
       return(
+      	content=(
+      	<Fragment >
           <div >
           <div>
             <div className="tweet">
-              <Bar
-                ref={this.myRef} 
-                onClick={()=> this.handleClick() } 
-                onChange={()=> this.handleChange()}
-              />
+            <Bar />
               <div className="upperBar">
                 	<InputTweet handleClick={this.handleClick}/>
               </div>
             </div>
-            </div>
 
-            <div className="actualTweet">
-             	<Feed tweets={array}  />
+          </div>
+
+            <div className="actualTweet" isLoaded={this.state.isLoaded /*hacer variable const al principio y utilizar variables*/}>
+             	<Feed tweets={tweets}  />
             </div>
           </div>
+         </Fragment>
+         )
       );
     }
+    return (
+      <div className="App">
+        { content }
+      </div>
+	)
+	}
 }
 
 
